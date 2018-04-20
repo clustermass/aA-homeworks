cf11adae96d31a33093b28098a9a7ebc3ccc086e
clustermass <clustermass2@gmail.com>
2018-04-19 23:03:41 -0700

    implement map

--- W1D5/homework.rb
+++ W1D5/homework.rb
@@ -63,3 +63,52 @@ queue.add(s3)
 p queue.remove
 p queue.remove
 p queue.remove
+
+
+class Map
+
+  def initialize
+    @map = Array.new
+  end
+
+  def assign(key,val)
+    if @map.none? {|kv_pair| kv_pair.first == key}
+      @map.push([key,val])
+    else
+      @map.each_with_index {|kv_pair,idx| kv_pair[1] = val if kv_pair.first == key}
+    end
+  end
+
+  def lookup(key)
+    return_val = @map.select {|kv_pair| kv_pair.first == key}.last
+  end
+
+  def remove(key)
+    @map.reject! {|kv_pair| kv_pair.first == key}
+  end
+
+  def show
+    self
+  end
+end
+
+p "test map"
+
+map = Map.new
+
+s1 = "first"
+s2 = "second"
+s3 = "third"
+# filling up
+map.assign(s1,1)
+map.assign(s2,2)
+map.assign(s3,3)
+p map.lookup(s3)
+p "trying to assign second key should overwrite value"
+map.assign(s3,100)
+p map.lookup(s3)
+p "trying to delete value by key"
+map.remove(s3)
+p map.lookup(s3)
+p "regular lookup"
+p map.lookup(s2)
